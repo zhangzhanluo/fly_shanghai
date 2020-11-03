70ded2572f504505d649dc51100d6b5366731117
zhanluo <zhangzhanluo@sjtu.edu.cn>
2020-11-03 08:45:39 +0800

    解决521代码报错问题，如出现该问题，把完整的请求表头复制进来即可。参考文章：https://blog.csdn.net/qq_27302597/article/details/79411808

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import time
 import json
 import requests
 from datetime import date, timedelta
+from copyheaders import headers_raw_to_dict
+
+import re  # 实现正则表达式
+import execjs  # 执行js代码
+
+user_agent_global = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+'Chrome/86.0.4240.111 Safari/537.36'
+url_global = 'https://www.shanghaiairport.com/ajax/flights/search.aspx?action=getData'
 
 
 def get_all_flights(direction='Departure'):
@@ -11,12 +19,24 @@ def get_all_flights(direction='Departure'):
     :param direction: Departure or Arrival
     :return: None but save the data as json file. See /Data/ folder.
     """
-    # Settings
-    url = 'https://www.shanghaiairport.com/ajax/flights/search.aspx?action=getData'
     # Pretend to be a browser. or the status code will be 403
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/86.0.4240.111 Safari/537.36'}
+        'User-Agent': user_agent_global}
+    headers_for_521 = b'''Accept: */*
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6
+Connection: keep-alive
+Content-Length: 130
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+Cookie: __jsluid_s=719744134dbd726e737da2e802314e54; Hm_lvt_d7682ab43891c68a00de46e9ce5b76aa=1603701341; shjc_indexNoticeHasRead=1; __jsl_clearance_s=1604362484.991|0|0llgShnUXtFaVn%2B5MtH4astjH6s%3D; ASP.NET_SessionId=e3liyv55hzssjxmwngl2he45; SERVERID=ad6585a6b63a4adcbf5c5362de3f1317|1604362495|1604362486
+Host: www.shanghaiairport.com
+Origin: https://www.shanghaiairport.com
+Referer: https://www.shanghaiairport.com/cn/flights.html
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36
+X-Requested-With: XMLHttpRequest'''
     payload = {'currentPage': 1,
                'pageSize': 20,
                'flightType': 1,
@@ -42,23 +62,30 @@ def get_all_flights(direction='Departure'):
     while True:
         payload['currentPage'] = i
         print('{}: Working on page {}'.format(direction, i))
-        r = requests.post(url, data=payload, headers=headers)
-        if r.status_code != 200:
+        r = requests.post(url_global, data=payload, headers=headers)
+
+        if r.status_code == 200:
+            pass
+        elif r.status_code == 521:
+            r = requests.post(url_global, data=payload, headers=headers_raw_to_dict(headers_for_521))
+            if r.status_code != 200:
+                raise NameError('521 - Status Code Error')
+        else:
             print('Status Code:', r.status_code)
             print(r.text)
             raise NameError('Status Code Error')
+
+        info = r.text
+        flights_info = info.split('$$$')[1]
+        if len(flights_info) == 0:
+            # Break until no records.
+            break
         else:
-            info = r.text
-            flights_info = info.split('$$$')[1]
-            if len(flights_info) == 0:
-                # Break until no records.
-                break
-            else:
-                flights = json.loads(flights_info)
-                all_flights += flights
-                i += 1
-                # 5 seconds will lead to 403 status code
-                time.sleep(20)
+            flights = json.loads(flights_info)
+            all_flights += flights
+            i += 1
+            # 5 seconds will lead to 403 status code
+            time.sleep(15)
 
     # Save the data
     yesterday = date.today() + timedelta(days=-1)
